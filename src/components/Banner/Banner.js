cc144245cabe4599e1fbca697dace4c902032b32
John <johndoe@example.com>
2024-05-13 11:42:16 +0200

    ajout alt

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,7 +4,7 @@ function Banner({image, text}) {
     return(
     <div className="banner-container">
         <h1 dangerouslySetInnerHTML={{__html: text}}></h1>
-        <img src={image}></img>
+        <img src={image} alt="la nature"></img>
     </div>
     )
 }
